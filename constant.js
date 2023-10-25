5a4e7d333ad4ccaa615bb183119e88870e57c2e1
goldentrash <startoflateness@gmail.com>
2023-10-25 22:00:00 +0900

    조정

--- constant.js
+++ constant.js
@@ -3,5 +3,5 @@ exports.NUM_OF_DATA_PER_PAGE = 50;
 
 exports.PORT = 8080;
 
-exports.LOST_SCHEDULE = { hour: 20, minute: 0 };
-exports.FOUND_SCHEDULE = { hour: 20, minute: 30 };
+exports.LOST_SCHEDULE = { hour: 21, minute: 0 };
+exports.FOUND_SCHEDULE = { hour: 19, minute: 30 };
