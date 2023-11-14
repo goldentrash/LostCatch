213db9fe2a1d9742176c7322082ce494564b7151
goldentrash <startoflateness@gmail.com>
2023-11-14 19:43:50 +0900

    우선 수동으로 fetch하도록 변경

--- fetch.js
+++ fetch.js
@@ -1,11 +1,14 @@
 const { scheduleJob } = require("node-schedule");
-const { FOUND_SCHEDULE, LOST_SCHEDULE } = require("../constant");
+const { FOUND_SCHEDULE, LOST_SCHEDULE } = require("./constant");
 
 const fetchFound = require("./schedules/fetchFoundData");
 const fetchLost = require("./schedules/fetchLostData");
 
-console.log(`schedule fetchFound at ${JSON.stringify(FOUND_SCHEDULE)}`);
-scheduleJob(FOUND_SCHEDULE, fetchFound);
+// 수동 동작하도록
+fetchFound().then(() => fetchLost().then(() => process.exit(0)));
 
-console.log(`schedule fetchLost at ${JSON.stringify(LOST_SCHEDULE)}`);
-scheduleJob(LOST_SCHEDULE, fetchLost);
+// console.log(`schedule fetchFound at ${JSON.stringify(FOUND_SCHEDULE)}`);
+// scheduleJob(FOUND_SCHEDULE, fetchFound);
+
+// console.log(`schedule fetchLost at ${JSON.stringify(LOST_SCHEDULE)}`);
+// scheduleJob(LOST_SCHEDULE, fetchLost);
